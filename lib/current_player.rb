aef4c95735a2f91ef80b084f538718d7682a3fba
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-02 06:00:23 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  count = 0
+  board.each do | val |
+    if val != "" || val != " "
+      count += 1
+    end
+  count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count%2 == 0
+    return "X"
+  end
+  return "O"
+end
